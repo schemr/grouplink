0c6ae29da412c2ab3c813d2ea8e65dce0ed6d4c1
schemr <schemryj@gmail.com>
2017-06-11 20:56:52 +0900

    Angular Material 테스트

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdButtonModule, MdCheckboxModule } from '@angular/material';
+
+import { MdCardModule, MdIconModule, MdButtonModule, MdCheckboxModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -17,7 +18,8 @@ import { AppComponent } from './app.component';
     HttpModule,
     BrowserAnimationsModule,
     MdButtonModule,
-    MdCheckboxModule
+    MdCheckboxModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
